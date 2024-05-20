10d55dc6b5be5af00db9701a278a4597b1e3e5bb
Jakub Sikorski <sikorski.jakubjan@gmail.com>
2024-05-20 17:01:07 +0200

    changed how we use the path

---
+++ src/lights/mod.rs
@@ -0,0 +1,178 @@
+use std::{collections::HashMap, net::SocketAddr, sync::Arc, time::Duration};
+
+use crate::states::LightState;
+use async_trait::async_trait;
+use coap_lite::{ContentFormat, MessageClass, ResponseType};
+use coap_server::app::{
+	CoapError, ObservableResource, Observers, ObserversHolder, Request, Response,
+};
+use serde_json::json;
+use tokio::{
+	sync::{oneshot, Mutex},
+	time,
+};
+
+#[derive(Default, Clone)]
+pub struct LightsState {
+	data: Arc<Mutex<HashMap<String, LightState>>>,
+	observers: ObserversHolder,
+}
+
+#[async_trait]
+impl ObservableResource for LightsState {
+	async fn on_active(&self, observers: Observers) -> Observers {
+		let relative_path = observers.relative_path();
+		println!("Observe active for path: {relative_path}...");
+		let attached = self.observers.attach(observers).await;
+		let (tx, mut rx) = oneshot::channel();
+		let observers = self.observers.clone();
+		tokio::spawn(async move {
+			let mut interval = time::interval(Duration::from_secs(5));
+			loop {
+				tokio::select! {
+					_ = &mut rx => {
+					   return
+					}
+					_ = interval.tick() => {
+						observers.notify_change().await;
+					}
+				}
+			}
+		});
+		attached.stay_active().await;
+		tx.send(()).unwrap();
+		println!("Observe no longer active for path: {relative_path}!");
+		attached.detach().await
+	}
+}
+
+pub async fn handle_get_lights(
+	request: Request<SocketAddr>,
+	state: LightsState,
+) -> Result<Response, CoapError> {
+	println!("registered request");
+	let mut response = request.new_response();
+	let light_id = &request.unmatched_path[0];
+
+	// let mut full_path = "".to_string();
+	println!("{}", light_id);
+	// for path in request.unmatched_path.into_iter() {
+	// 	full_path += &path;
+	// }
+	match state.data.lock().await.get(light_id) {
+		Some(x) => {
+			response
+				.message
+				.set_content_format(ContentFormat::TextPlain);
+			response.message.payload = serde_json::to_string(x).unwrap().as_bytes().to_vec();
+			Ok(response)
+		}
+		None => {
+			response.set_status(ResponseType::NotFound);
+			response.message.header.code = MessageClass::Response(ResponseType::NotFound);
+			Ok(response)
+		}
+	}
+}
+
+pub async fn handle_is_online(
+	request: Request<SocketAddr>,
+	state: LightsState,
+) -> Result<Response, CoapError> {
+	let mut response = request.new_response();
+	let light_id = &request.unmatched_path[0];
+	// let mut full_path = "".to_string();
+	// for path in request.unmatched_path.into_iter() {
+	// 	full_path += &path;
+	// }
+	let relative_path = format!("lights/{}", light_id);
+	println!("{}", relative_path);
+	// Wont work with stock library
+	let observer_count = state.observers.get_count(&relative_path).await;
+	println!("{}", observer_count);
+	match state.data.lock().await.get(light_id) {
+		Some(_) => {
+			response
+				.message
+				.set_content_format(ContentFormat::TextPlain);
+			response.message.payload = serde_json::to_string(&json!({"isOnline":observer_count>0}))
+				.unwrap()
+				.as_bytes()
+				.to_vec();
+			Ok(response)
+		}
+		None => {
+			response.set_status(ResponseType::NotFound);
+			response.message.header.code = MessageClass::Response(ResponseType::NotFound);
+			Ok(response)
+		}
+	}
+}
+
+// Changing the light state
+pub async fn handle_put_lights(
+	request: Request<SocketAddr>,
+	state: LightsState,
+) -> Result<Response, CoapError> {
+	let mut response = request.new_response();
+	let light_id = &request.unmatched_path[0];
+	// let mut full_path = "".to_string();
+	// for path in request.unmatched_path.clone().into_iter() {
+	// 	full_path += &path;
+	// }
+	println!(
+		"{}",
+		String::from_utf8(request.original.message.payload.clone()).unwrap()
+	);
+	let new_state: LightState =
+		serde_json::from_str(&String::from_utf8(request.original.message.payload).unwrap())
+			.unwrap();
+	let device_id = request.unmatched_path.last().unwrap();
+	let mut data = state.data.lock().await;
+	println!("handling put");
+	let data_clone = data.clone();
+	let current_state = data_clone.get(device_id);
+	if current_state.is_none() {
+		println!("device not found");
+		return Err(CoapError::not_found());
+	}
+	data.insert(device_id.clone(), new_state);
+	println!("{device_id}");
+	let full_path = format!("lights/{}", light_id);
+	println!("{}", full_path);
+	response
+		.message
+		.set_content_format(ContentFormat::TextPlain);
+	state.observers.notify_change_for_path(&full_path).await;
+	Ok(response)
+}
+
+// Creating the device
+pub async fn handle_device_create_put(
+	request: Request<SocketAddr>,
+	state: LightsState,
+) -> Result<Response, CoapError> {
+	println!("Creating device");
+	let mut response = request.new_response();
+	let mut full_path = "".to_string();
+	let payload = String::from_utf8(request.original.message.payload).unwrap();
+	println!("{}", payload.as_ref() as &str);
+	for path in request.unmatched_path.clone().into_iter() {
+		full_path += &path;
+	}
+	let mut data = state.data.lock().await;
+	let len = data.len() + 1;
+	data.insert(
+		payload,
+		LightState {
+			is_on: true,
+			color: 255 * 255 * 255,
+			brightness: 255,
+		},
+	);
+	response
+		.message
+		.set_content_format(ContentFormat::TextPlain);
+	response.message.payload = len.to_string().as_bytes().to_vec();
+	Ok(response)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,19 @@
-use std::collections::HashMap;
 use std::net::SocketAddr;
-use std::sync::Arc;
-use std::time::Duration;
 
-use async_trait::async_trait;
+use crate::lights::LightsState;
 use coap_lite::link_format::LINK_ATTR_RESOURCE_TYPE;
-use coap_lite::{ContentFormat, MessageClass, ResponseType};
-use serde_json::json;
-use tokio::sync::{oneshot, Mutex};
-use tokio::time;
-
-use coap_server::app::ObservableResource;
-use coap_server::app::{AppBuilder, CoapError, Observers, ObserversHolder, Request, Response};
+use coap_server::app::AppBuilder;
 use coap_server::FatalServerError;
 use coap_server::{app, CoapServer};
 use coap_server_tokio::transport::udp::UdpTransport;
 use dotenv::dotenv;
 use env_logger;
+use lights::{handle_device_create_put, handle_get_lights, handle_is_online, handle_put_lights};
 #[macro_use]
 extern crate serde_derive;
+
+pub mod lights;
+pub mod states;
 
 #[tokio::main]
 async fn main() -> Result<(), FatalServerError> {
@@ -56,185 +51,4 @@ fn build_app() -> AppBuilder<SocketAddr> {
 			app::resource("/lights/is_online")
 				.get(move |req| handle_is_online(req, state_for_is_online.clone())),
 		)
-}
-
-pub mod states;
-
-use crate::states::LightState;
-
-#[derive(Default, Clone)]
-struct LightsState {
-	data: Arc<Mutex<HashMap<String, LightState>>>,
-	observers: ObserversHolder,
-}
-
-#[async_trait]
-impl ObservableResource for LightsState {
-	async fn on_active(&self, observers: Observers) -> Observers {
-		let relative_path = observers.relative_path();
-		//let device_id = relative_path.split('/').last().unwrap().to_string();
-		println!("Observe active for path: {relative_path}...");
-		let attached = self.observers.attach(observers).await;
-		let (tx, mut rx) = oneshot::channel();
-		let observers = self.observers.clone();
-		//let mut data = self.data.clone();
-		tokio::spawn(async move {
-			let mut interval = time::interval(Duration::from_secs(5));
-			loop {
-				tokio::select! {
-					_ = &mut rx => {
-					   return
-					}
-					_ = interval.tick() => {
-						observers.notify_change().await;
-					}
-				}
-			}
-		});
-		attached.stay_active().await;
-		tx.send(()).unwrap();
-		println!("Observe no longer active for path: {relative_path}!");
-		attached.detach().await
-	}
-}
-
-async fn handle_get_lights(
-	request: Request<SocketAddr>,
-	state: LightsState,
-) -> Result<Response, CoapError> {
-	println!("registered request");
-	let mut response = request.new_response();
-	let mut full_path = "".to_string();
-	for path in request.unmatched_path.into_iter() {
-		full_path += &path;
-	}
-	// println!(
-	// 	"{}",
-	// 	state
-	// 		.observers
-	// 		.get_count(format!("lights/{}", &full_path).as_str())
-	// 		.await
-	// );
-	match state.data.lock().await.get(full_path.as_str()) {
-		Some(x) => {
-			response
-				.message
-				.set_content_format(ContentFormat::TextPlain);
-			response.message.payload = serde_json::to_string(x).unwrap().as_bytes().to_vec();
-			Ok(response)
-		}
-		None => {
-			response.set_status(ResponseType::NotFound);
-			response.message.header.code = MessageClass::Response(ResponseType::NotFound);
-			Ok(response)
-		}
-	}
-	// println!("{full_path}");
-	// full_path = format!("lights/{full_path}");
-}
-
-async fn handle_is_online(
-	request: Request<SocketAddr>,
-	state: LightsState,
-) -> Result<Response, CoapError> {
-	let mut response = request.new_response();
-	let mut full_path = "".to_string();
-	for path in request.unmatched_path.into_iter() {
-		full_path += &path;
-	}
-	let relative_path = format!("lights/{}", &full_path);
-	println!("{}", relative_path);
-	// Wont work with stock library
-	let observer_count = state.observers.get_count(&relative_path).await;
-	println!("{}", observer_count);
-	//state.observers.get_count(relative_path.as_str()).await;
-	// if observer_count > 0 {
-	// 	println!("hii");
-	// }
-	match state.data.lock().await.get(full_path.as_str()) {
-		Some(x) => {
-			response
-				.message
-				.set_content_format(ContentFormat::TextPlain);
-			response.message.payload = serde_json::to_string(&json!({"isOnline":observer_count>0}))
-				.unwrap()
-				.as_bytes()
-				.to_vec();
-			Ok(response)
-		}
-		None => {
-			response.set_status(ResponseType::NotFound);
-			response.message.header.code = MessageClass::Response(ResponseType::NotFound);
-			Ok(response)
-		}
-	}
-	// println!("{full_path}");
-	// full_path = format!("lights/{full_path}");
-}
-
-// Changing the light state
-async fn handle_put_lights(
-	request: Request<SocketAddr>,
-	state: LightsState,
-) -> Result<Response, CoapError> {
-	let mut response = request.new_response();
-	let mut full_path = "".to_string();
-	for path in request.unmatched_path.clone().into_iter() {
-		full_path += &path;
-	}
-	println!(
-		"{}",
-		String::from_utf8(request.original.message.payload.clone()).unwrap()
-	);
-	let new_state: LightState =
-		serde_json::from_str(&String::from_utf8(request.original.message.payload).unwrap())
-			.unwrap();
-	let device_id = request.unmatched_path.last().unwrap();
-	let mut data = state.data.lock().await;
-	println!("handling put");
-	let data_clone = data.clone();
-	let current_state = data_clone.get(device_id);
-	if current_state.is_none() {
-		println!("device not found");
-		return Err(CoapError::not_found());
-	}
-	data.insert(device_id.clone(), new_state);
-	println!("{device_id}");
-	full_path = format!("lights/{full_path}");
-	println!("{}", full_path);
-	response
-		.message
-		.set_content_format(ContentFormat::TextPlain);
-	state.observers.notify_change_for_path(&full_path).await;
-	Ok(response)
-}
-
-// Creating the device
-async fn handle_device_create_put(
-	request: Request<SocketAddr>,
-	state: LightsState,
-) -> Result<Response, CoapError> {
-	println!("Creating device");
-	let mut response = request.new_response();
-	let mut full_path = "".to_string();
-	let payload = String::from_utf8(request.original.message.payload).unwrap();
-	println!("{}", payload.as_ref() as &str);
-	for path in request.unmatched_path.clone().into_iter() {
-		full_path += &path;
-	}
-	let mut data = state.data.lock().await;
-	let len = data.len() + 1;
-	data.insert(
-		payload,
-		LightState {
-			is_on: true,
-			color: 255 * 255 * 255,
-			brightness: 255,
-		},
-	);
-	response
-		.message
-		.set_content_format(ContentFormat::TextPlain);
-	response.message.payload = len.to_string().as_bytes().to_vec();
-	Ok(response)
 }
